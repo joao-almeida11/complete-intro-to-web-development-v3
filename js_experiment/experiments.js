8300998a9962289dddf97907631a6bab3fe6b051
Joao Almeida <jopecoal@gmail.com>
2025-03-04 17:45:05 +0000

    feat: loop exercise

---
+++ js_experiment/experiments.js
@@ -0,0 +1,14 @@
+const timesToRepeat = 10;
+const character = '🐶';
+
+// some loop that takes that character and repeats it that many times
+
+// write your answer here
+let answer = '';
+for (let i = 0; i < timesToRepeat; i++) {
+    answer = answer + character;
+}
+
+console.log(answer);
+
+// '🐶🐶🐶🐶🐶🐶🐶🐶🐶🐶'; (10)
